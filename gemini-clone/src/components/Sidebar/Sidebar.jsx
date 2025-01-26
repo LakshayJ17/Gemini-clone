43edd3cda6e05ebc97de4ecee800d20a23bc874f
lakshay17 <lakshayjain01lj@gmail.com>
2025-01-26 17:47:35 +0530

    Enhance Main and Sidebar components with loading state and recent prompts functionality; improve styling for results display

--- gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { Context } from '../../context/Context'
 
 function Sidebar() {
 
     const [extended, setExtended] = useState(false)
+    const { onSent, prevPrompts, setRecentPrompt } = useContext(Context)
 
     return (
         <div className='sidebar'>
@@ -17,10 +19,15 @@ function Sidebar() {
 
                 {extended ? <div className="recent">
                     <p className="recent-title">Recent</p>
-                    <div className="recent-entry">
-                        <img src={assets.message_icon} alt="" />
-                        <p>What is react ...</p>
-                    </div>
+                    {prevPrompts.map((item, index) => {
+                        return (
+                            <div key={index} className="recent-entry">
+                                <img src={assets.message_icon} alt="" />
+                                <p>{item.slice(0,16)} ...</p>
+                            </div>
+                        )
+                    })}
+
                 </div> : null}
 
             </div>
--- gemini-clone/src/context/Context.jsx
+++ gemini-clone/src/context/Context.jsx
@@ -1,9 +1,9 @@
-import { useState,createContext } from "react";
+import { useState, createContext } from "react";
 import run from '../config/gemini'
 
 export const Context = createContext();
 
-const ContextProvider = (props) =>{
+const ContextProvider = (props) => {
     const [input, setInput] = useState('')
     const [recentPrompt, setRecentPrompt] = useState('')
     const [prevPrompts, setPrevPrompts] = useState([])
@@ -11,15 +11,44 @@ const ContextProvider = (props) =>{
     const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState("")
 
+    // Typing effect - This will add a delay of 75ms for each word
+    const delayPara = (index, nextWord) => {
+        setTimeout(() => {
+            setResultData(prev => prev + nextWord)
+        }, 75 * index);
+    }
 
-    const onSent = async () =>{
-        setResultData("")
-        setLoading(true)
-        setShowResult(true)
-        setRecentPrompt(input)
-        const response = await run(input)
-        setResultData(response)
-        setLoading(false)
+    const onSent = async () => {
+        setResultData("")                               // Clear the previous result
+        setLoading(true)                                // Show the loader
+        setShowResult(true)                             // Show the result
+        setRecentPrompt(input)                          // Set the recent prompt
+        setPrevPrompts(prev => [...prev, input])        // Add the input to the list of previous prompts
+
+        const response = await run(input) // This will return the response from the AI
+
+        // The output is somethings like xyz is a **abc** which is def*ghi*klm
+        // This will convert the 2 stars to bold
+        let responseArray = response.split("**")
+        let newResponse = "";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 != 1) {
+                newResponse += responseArray[i]
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>"
+            }
+        }
+        // This will convert the 1 star to break/new line
+        let newResponse2 = newResponse.split("*").join("<br>")
+
+        // For typing effect - Split the response into words 
+        let newResponseArray = newResponse2.split(" ")
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i]
+            delayPara(i, nextWord + " ")
+        }
+
+        setLoading(false)               // Hide the loader
         setInput("")
 
     }
